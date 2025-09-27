51b983c08ab65f1ca2635ad9a09ea8fd1309d2ff
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-27 16:53:57 +0100

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,19 @@
 'use client'
 
-import './globals.css'
-import { AppKitProvider } from '@reown/appkit/react'
-import { appKit } from '../lib/wallet'
+import { useEffect } from 'react'
 import Navbar from '../components/Navbar'
+import { walletManager } from '../lib/BaseWalletManager'
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    walletManager.initializeAppKit()
+  }, [])
+
   return (
     <html lang="en">
-      <body>
-        {/* Wrap app with AppKitProvider so hooks work */}
-        <AppKitProvider appKit={appKit}>
-          <Navbar />
-          <main className="p-4">{children}</main>
-        </AppKitProvider>
+      <body className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1 container mx-auto px-4 py-6">{children}</main>
       </body>
     </html>
   )
