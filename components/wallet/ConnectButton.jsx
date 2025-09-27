6c7492bf6c61268f3505a5a3d129bc4923d82576
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-27 23:58:55 +0100

    cb

--- components/wallet/ConnectButton.jsx
+++ components/wallet/ConnectButton.jsx
@@ -1,29 +1,12 @@
 'use client'
-
-import { useState } from 'react'
-import { walletManager } from '../lib/BaseWalletManager'
+import React from 'react'
+import { ConnectButton as ReownConnect } from '@reown/appkit/react'
 
 export default function ConnectButton() {
-  const [address, setAddress] = useState(null)
-
-  const handleConnect = async () => {
-    try {
-      await walletManager.openModal()
-      const account = await walletManager.getAccount()
-      if (account.isConnected) setAddress(account.address)
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
+  // Reown AppKit's ConnectButton handles wallet selection (WalletConnect, MetaMask, etc.)
   return (
-    <button
-      onClick={handleConnect}
-      className="px-4 py-2 bg-white text-blue-600 rounded hover:bg-gray-100 font-semibold"
-    >
-      {address
-        ? `${address.slice(0, 6)}...${address.slice(-4)}`
-        : 'Connect Wallet'}
-    </button>
+    <div>
+      <ReownConnect />
+    </div>
   )
 }
