c2df777089d8bb3c8e52d83f4acdfb5e687f55b7
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-28 00:48:07 +0100

    update

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react'
 import Link from 'next/link'
-import ConnectButton from '../../components/wallet/ConnectButton'
+import ConnectButton from '../components/wallet/ConnectButton'
 
 export default function HomePage() {
   return (
