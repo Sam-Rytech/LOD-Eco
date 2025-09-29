cdb2aa2ac9fc08124e1ee832c0de47fe78cb72ca
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-29 03:17:47 +0100

    bah

--- app/stake/page.jsx
+++ app/stake/page.jsx
@@ -1,24 +1,9 @@
-'use client'
-import React from 'react'
-import StakeForm from '../../components/staking/StakeForm'
-import UnstakeForm from '../../components/staking/UnstakeForm'
-import RewardsDisplay from '../../components/staking/RewardsDisplay'
+import StakeForm from '../../components/StakeForm';
 
 export default function StakePage() {
   return (
-    <div className="space-y-6">
-      <h2 className="text-xl font-semibold">Staking</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="bg-white p-6 rounded shadow">
-          <StakeForm />
-        </div>
-        <div className="bg-white p-6 rounded shadow">
-          <UnstakeForm />
-          <div className="mt-4">
-            <RewardsDisplay />
-          </div>
-        </div>
-      </div>
+    <div className="grid gap-6">
+      <StakeForm />
     </div>
-  )
+  );
 }
