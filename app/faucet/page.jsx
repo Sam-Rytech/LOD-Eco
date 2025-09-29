571b4b11b6d698036688e3ddf1c46278ba6f97b0
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-29 03:17:21 +0100

    hsu

--- app/faucet/page.jsx
+++ app/faucet/page.jsx
@@ -1,24 +1,9 @@
-'use client'
-import React from 'react'
-import FaucetButton from '../../components/faucet/FaucetButton'
-import FaucetInfo from '../../components/faucet/FaucetInfo'
-import FaucetFundForm from '../../components/faucet/FaucetFundForm'
+import FaucetForm from '../../components/FaucetForm';
 
 export default function FaucetPage() {
   return (
-    <div className="space-y-6">
-      <h2 className="text-xl font-semibold">Faucet</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="bg-white p-6 rounded shadow">
-          <FaucetInfo />
-          <div className="mt-4">
-            <FaucetButton />
-          </div>
-        </div>
-        <div className="bg-white p-6 rounded shadow">
-          <FaucetFundForm />
-        </div>
-      </div>
+    <div className="grid gap-6">
+      <FaucetForm />
     </div>
-  )
+  );
 }
