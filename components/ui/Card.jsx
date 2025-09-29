87c9fd625efa6072da254b14f93e15812b9694c5
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-29 03:12:00 +0100

    Restructure project

--- components/ui/Card.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Card({ children, className = '' }) {
-  return (
-    <div className={`p-4 bg-white rounded shadow ${className}`}>{children}</div>
-  )
-}
